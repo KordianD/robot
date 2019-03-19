906f2774ffc052d2d0a194cb1ad6ab22c9fce053
KordianD <kordiandrozd@gmail.com>
2019-03-19 22:54:38 +0100

    Added ability to run server

--- src/app.py
+++ src/app.py
@@ -1,6 +1,11 @@
 from flask import Flask
-app = Flask(__name__)
+APP = Flask(__name__)
 
-@app.route("/")
+
+@APP.route("/")
 def hello():
     return "Hello World!"
+
+
+if __name__ == "__main__":
+    APP.run(host='0.0.0.0', debug=True)
