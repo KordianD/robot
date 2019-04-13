8c69981c0fffc61e355935a082707a40a0533ce7
AdamLatos <latosadam1@gmail.com>
2019-04-13 18:56:42 +0200

    add robot age and velocity

--- src/robot.py
+++ src/robot.py
@@ -13,6 +13,8 @@ class Robot:
         self.socket.connect((mac, 1))
         self.last_update = time.time()
         self.color = color
+        self.new_player_time = time.time()
+        self.player_age = 15
 
     def send_speed_command(self, left, right):
         cmd = '[={},{}]'.format(left, right)
@@ -21,19 +23,30 @@ class Robot:
         print(cmd)
 
     def forward(self):
-        self.send_speed_command(DEFAULT_VELOCITY, DEFAULT_VELOCITY)
+        self.send_speed_command(self.velocity(), self.velocity())
 
     def reverse(self):
-        self.send_speed_command(-DEFAULT_VELOCITY, -DEFAULT_VELOCITY)
+        self.send_speed_command(-self.velocity(), -self.velocity())
 
     def left(self):
-        self.send_speed_command(0, DEFAULT_VELOCITY)
+        self.send_speed_command(0, math.ceil(self.velocity()/2))
 
     def right(self):
-        self.send_speed_command(DEFAULT_VELOCITY, 0)
+        self.send_speed_command(math.ceil(self.velocity()/2), 0)
 
     def stop(self):
         self.send_speed_command(0, 0)
 
     def is_time_exceeded(self):
         return (time.time() - self.last_update) > TIME_INTERVAL
+
+    def robot_age(self):
+        return (time.time() - self.new_player_time)
+
+    def velocity(self):
+        if player_age < 10:
+            return math.ceil(DEFAULT_VELOCITY/2)
+        elif player_age > 20:
+            return DEFAULT_VELOCITY
+        else
+        return math.ceil(player_age / 20 * DEFAULT_VELOCITY)
